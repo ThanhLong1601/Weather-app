9dd2fc33dd96ebd47a854a7d6a9882e093e6e56e
Thanh Long <thanhlong160100@gmail.com>
2024-11-29 09:42:53 +0700

    first commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require('postman-request')
+require('dotenv').config();
+
+const geocodingApiKey = process.env.GEOCODE_API_KEY;
+
+const geocode = (address, callback) => {
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(address)}&key=${geocodingApiKey}`;
+
+    request({url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to geocoding service!!!', undefined)
+        } else if (body.results.length === 0) {
+            callback('This address was not found. Try another search', undefined)
+        } else {
+            callback(undefined, {
+                location: body.results[0].formatted,
+                latitude: body.results[0].geometry.lat,
+                longitude: body.results[0].geometry.lng
+            })
+        }
+    })
+}
+
+module.exports = geocode
---
+++ src/utils/weatherAPI.js
@@ -0,0 +1,25 @@
+const request = require('postman-request')
+require('dotenv').config();
+
+const weatherApiKey = process.env.WEATHER_API_KEY
+
+const weatherAPI = (lat, lng, callback) => {
+    const url = `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${lat},${lng}`
+
+    request({url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!!!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+            console.log(body.error)
+        } else {
+            callback(undefined, body.current.condition.text
+                + '. It is currently ' 
+                + body.current.temp_c 
+                + ' degree out. There is a ' 
+                + body.current.precip_mm + 'mm change of rain')
+        }
+    })
+}
+
+module.exports = weatherAPI
