f0e09055cec2cfc46ddb936dd07571250e52cd18
Thanh Long <thanhlong160100@gmail.com>
2024-12-01 17:59:12 +0700

    Use dynamic port for deloyment

--- src/app.js
+++ src/app.js
@@ -95,6 +95,7 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is on port 3000.')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is on port ${port}.`)
 })
